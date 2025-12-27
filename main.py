f9358a7205d9c52d8a244f1f832fedaa623ad0b9
Apireak ROEUN <95199246+roeunapireak@users.noreply.github.com>
2025-12-27 15:44:11 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+from pygame import *
+
+
+''' colors '''
+background = (200,255,255)  # Red/Green/Blue
+
+
+''' variables '''
+width = 700
+height = 500
+
+
+window = display.set_mode( (width, height) )
+window.fill(background)
+
+clock = time.Clock()
+
+
+game = True
+
+
+
+
+''' game loop '''
+while game: 
+
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+
+
+    display.update()
+    clock.tick(60)
+
+
+
