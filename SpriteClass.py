d4cdd5e28746b4d34039ac5b14ce962e53110071
RoeunApireak <apireak.roeun.datau@gmail.com>
2026-02-22 10:41:48 +0700

    changes

--- SpriteClass.py
+++ SpriteClass.py
@@ -1,16 +1,23 @@
 from pygame import *
 
 class GameSprite(sprite.Sprite):
+    
     ''' constructor ''' 
-    def __init__(self, player_image, player_x, player_y, image_width, image_height, player_speed):
+    def __init__(self, player_image, player_x, player_y, 
+                 image_width, image_height, player_speed):
         super().__init__()
-        self.image = transform.scale(image.load(player_image), (image_width, image_height))
+
+        self.image = transform.scale(image.load(player_image), 
+                                     (image_width, image_height))
+        
         self.rect = self.image.get_rect()
         self.rect.x = player_x
         self.rect.y = player_y
+
         self.speed = player_speed
 
         self.window = None
+
 
     def reset(self, window):
         self.window = window
@@ -18,18 +25,4 @@ class GameSprite(sprite.Sprite):
 
 
 class Player(GameSprite):
-    
-    def update_left(self):
-        keys = key.get_pressed()
-        if keys[K_w] and self.rect.y > 2:
-            self.rect.y -= self.speed
-        if keys[K_s] and self.rect.y < 435:
-            self.rect.y += self.speed
-
-    def update_right(self):
-        keys = key.get_pressed()
-        if keys[K_UP] and self.rect.y > 2:
-            self.rect.y -= self.speed
-        if keys[K_DOWN] and self.rect.y < 435:
-            self.rect.y += self.speed
-
+    pass
--- main.py
+++ main.py
@@ -1,86 +1,31 @@
 from pygame import *
-from SpriteClass import GameSprite, Player
+from spriteClass import GameSprite
 
-''' colors '''
-background = (200,255,255)  # Red/Green/Blue
-
-
-''' variables '''
+background = (200,255,255)
 width = 700
 height = 500
 
-
 window = display.set_mode( (width, height) )
-# window.fill(background)
+display.set_caption('Ping Pong Game')
 
 clock = time.Clock()
 
-
-ball = GameSprite('./images/ball.png', width/2, height/2, 50,50, 3)
-racket_left = Player('./images/racket_left.png', 2, height/2, 65,65, 3)
-racket_right = Player('./images/Modified/racket_right.png', width-65, height/2, 65,65, 3)
-
+ball = GameSprite('./images/ball.png', 
+                  width/2, height/2, 
+                  50,50, 3)
 
 game = True
 
-ball_speed_x = ball.speed
-ball_speed_y = ball.speed
+while game:
 
-font.init()
-font1 = font.Font(None, 35)
-lose_player_left = font1.render('LEFT PLAYER LOSES!', True, (180, 0, 0))
-lose_player_right = font1.render('RIGHT PLAYER LOSES!', True, (180, 0, 0))
+    window.fill(background)
 
-finish = True
-
-''' game loop '''
-while game: 
-
-    if finish :
-        window.fill(background)
-
-        ball.reset(window)
-        ball.rect.x -= ball_speed_x
-        ball.rect.y -= ball_speed_y
-
-        if ball.rect.y <= 2 or ball.rect.y >= 400:
-            ball_speed_y *= -1
-        if sprite.collide_rect(ball,racket_left) or sprite.collide_rect(ball, racket_right):
-            ball_speed_x *= -1
-
-        # win & lose 
-        if ball.rect.x <= 0:
-            window.blit(lose_player_left, ( (width/2)-90, (height/2)-30 ))
-            finish = False
-
-        if ball.rect.x >= (width-65):
-            window.blit(lose_player_right, ( (width/2)-90, (height/2)-30 ))
-            finish = False
-
-
-        racket_left.reset(window)
-        racket_left.update_left()
-
-        racket_right.reset(window)
-        racket_right.update_right()
-
-    else:
-        
-        time.delay(1000)
-
-        ball.rect.x = width/2
-        ball.rect.y = height/2
-
-        finish = True
+    ball.reset(window)
 
 
     for e in event.get():
         if e.type == QUIT:
             game = False
 
-
     display.update()
     clock.tick(60)
-
-
-
